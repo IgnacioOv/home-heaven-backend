e3cb2ab97b464e5787969e666faa07393b8332ad
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-05-10 20:05:00 -0300

    first commit

---
+++ src/main/java/com/homeheaven/backend/HomeHeavenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.homeheaven.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHeavenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeHeavenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeheaven/backend/HomeHeavenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeheaven.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeHeavenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
