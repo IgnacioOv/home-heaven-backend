320c1c252f349a5132868ec63e35532e1f44ca05
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-05-17 21:41:28 -0300

    feat: adds user controller and its methods with its database connection

---
+++ src/main/java/com/homeheaven/backend/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.homeheaven.backend.controller;
+
+
+import com.homeheaven.backend.entity.User;
+import com.homeheaven.backend.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User savedUser = userService.addUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable int userId) {
+        User user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/homeheaven/backend/service/UserService.java
@@ -0,0 +1,26 @@
+package com.homeheaven.backend.service;
+
+import com.homeheaven.backend.entity.User;
+import com.homeheaven.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(int userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+}
