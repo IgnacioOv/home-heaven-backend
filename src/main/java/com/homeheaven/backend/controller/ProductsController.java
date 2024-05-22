79b201dd58b9f61886758cbdfa27934121a27f5a
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-05-22 18:03:09 -0300

    feat: adds product methods and changes the data source

---
+++ src/main/java/com/homeheaven/backend/controller/ProductsController.java
@@ -0,0 +1,36 @@
+package com.homeheaven.backend.controller;
+
+
+import com.homeheaven.backend.entity.Product;
+import com.homeheaven.backend.service.ProductsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@AllArgsConstructor
+@RestController("products")
+public class ProductsController {
+
+    private ProductsService productsService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        Product newProduct = productsService.addProduct(product);
+        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<Product>> getAllProducts() {
+        Iterable<Product> products = productsService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable int productId) {
+        productsService.deleteProduct(productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/main/java/com/homeheaven/backend/entity/Product.java
@@ -0,0 +1,44 @@
+package com.homeheaven.backend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_description")
+    private String productDescription;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @Column(name = "price")
+    private float price;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "seller_id")
+    private long sellerId;
+
+
+
+}
